0d1dc3d78191832760e314c67da7c017e4e5c80e
GonzaCC <acanazac@uni.pe>
2022-08-01 04:57:25 -0500

    primer desafio

--- src/App.js
+++ src/App.js
@@ -5,17 +5,18 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
+        {/* <img src={logo} className="App-logo" alt="logo" /> */}
+        <img src="https://i.pinimg.com/736x/37/8a/27/378a270e775265622393da8c0527417e.jpg" className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          Hola, soy Gonzalo, este es el primer desafío.
         </p>
         <a
           className="App-link"
-          href="https://reactjs.org"
+          href="https://github.com/gonzccode"
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          Mi repositorio
         </a>
       </header>
     </div>
