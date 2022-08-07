cc21544429264800da23ba2baf3c7687f452f835
GonzaCC <acanazac@uni.pe>
2022-08-06 19:47:47 -0500

    agregando navbar

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -7,7 +7,7 @@ function NavBar(){
         <nav>
             <div class="nav-wrapper">
                 <img src="https://i.pinimg.com/736x/37/8a/27/378a270e775265622393da8c0527417e.jpg" className="NavBarLogo" />
-                <a href="#!" class="brand-logo">Project</a>
+                <a href="#!" class="brand-logo">Project Estudent</a>
                 <ul class="right hide-on-med-and-down">
                     <li><a href="#">Inicio</a></li>
                     <li><a href="https://www.linkedin.com/in/gcanazacupe/">Contacto</a></li>
